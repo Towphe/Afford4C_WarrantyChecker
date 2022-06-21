720b198781b15eb9385a5f5ef3b0e3df2fa05fbf
Christopher James Castillo <castillochristopherjames@gmail.com>
2022-06-21 10:20:55 +0800

    Added access to user DB

---
+++ Backend/Repo/AppUserDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace Repo
+{
+    public partial class AppUserDbContext : IdentityDbContext<AppUser>
+    {
+        public AppUserDbContext(DbContextOptions<AppUserDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Backend/Repo/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Repo
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppUserDbContext>
+    {
+        public DesignTimeDbContextFactory()
+        {
+        }
+        public AppUserDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppUserDbContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Database=WarrantyCheckerAppUserDB;Username=postgres;Password=pingu");
+            return new AppUserDbContext(optionsBuilder.Options);
+        }
+    }
+}
