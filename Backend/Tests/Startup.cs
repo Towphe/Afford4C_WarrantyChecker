828b7ff5be26d91914785dd21a201227daec2cbb
Christopher James Castillo <castillochristopherjames@gmail.com>
2022-06-24 22:30:53 +0800

    Setup DB Context for Test Project

---
+++ Backend/Tests/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repo;
+
+namespace Tests
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppUserDbContext>(opts => opts.UseInMemoryDatabase("testDB"));
+            services.AddDbContext<WarrantyCheckerAppDBContext>(opts => opts.UseInMemoryDatabase("testDB"));
+        }
+    }
+}
